f746bd6125f768205c97ac282af433daf44c0438
kei-tamiya <webcreatorhouse@gmail.com>
2017-04-29 17:15:59 +0900

    add untracked files

---
+++ config/webpack/loaders/vue.js
@@ -0,0 +1,15 @@
+module.exports = {
+  test: /.vue$/,
+  loader: 'vue-loader',
+  options: {
+    loaders: {
+        js: 'babel-loader',
+        file: 'file-loader',
+        scss: 'vue-style-loader!css-loader!postcss-loader!sass-loader',
+        sass: 'vue-style-loader!css-loader!postcss-loader!sass-loader?indentedSyntax'
+    },
+    presets: [
+        ['es2015']
+    ]
+  }
+}
