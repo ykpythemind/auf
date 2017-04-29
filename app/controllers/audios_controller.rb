9333ccc39744f4fef17c555ab6347dd96e01f016
Yukito Ito <yukibukiyou@gmail.com>
2017-04-29 17:07:40 +0900

    UserモデルとAudioモデル作成　/ POSTだけできるように

---
+++ app/controllers/audios_controller.rb
@@ -0,0 +1,39 @@
+class AudiosController < ApplicationController
+  before_action :set_audio, only: [:show, :edit, :update, :destroy]
+
+  def index
+    render json: {title: 'jsonが返っている'}
+  end
+
+  def show
+    # 今は仮にjsonが返ります
+    render json: @audio
+  end
+
+  def new
+
+  end
+
+  def create
+    @audio = Audio.new(audio_params)
+
+    respond_to do |format|
+      if @audio.save
+        format.html { redirect_to root_path, notice: '投稿しました' }
+        format.json { render json: @user, status: :created}
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_audio
+      @audio = Audio.find(params[:id])
+    end
+
+    def audio_params
+      params.require(:audio).permit(:user_id, :update_time, :title, :description, :filename, :length)
+    end
+end
---
+++ spec/factories/audios.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :audio do
+    user_id 1
+    update_time "2017-04-29 15:49:37"
+    title "MyString"
+    description "MyText"
+    filename "MyString"
+    length 1
+  end
+end
