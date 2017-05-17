5da067450a0f921c3841381a5641a40e6c8739fd
kei-tamiya <webcreatorhouse@gmail.com>
2017-05-17 19:24:46 +0900

    [fix] App.vueが二重で読み込まれる問題の修正、マウント方法を変更

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,21 +1,18 @@
 import Vue from 'vue';
 import VuexRouterSync from 'vuex-router-sync';
 
-import App from './App.vue';
 import router from './router';
 import store from './store';
 
 VuexRouterSync.sync(store, router);
 
-// TODO dotenv入れて環境分ける
-// if(process.env.NODE_ENV !== 'production') {
-//     Vue.config.devtools = true;
-// }
+if (process.env.NODE_ENV !== 'production') {
+  Vue.config.devtools = true;
+}
 
-document.body.appendChild(document.createElement('app'));
 /* eslint-disable no-new */
 new Vue({
+  el: 'router-view',
   router,
-  store,
-  render: h => h(App)
-}).$mount('app');
+  store
+});
