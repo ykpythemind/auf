314bbe794ecc43bf4c70aeadf6517611a721db59
Yukito Ito <yukibukiyou@gmail.com>
2017-05-20 15:10:36 +0900

    UploadFormでイベントをbind

--- app/javascript/packs/components/uploadform.vue
+++ app/javascript/packs/components/uploadform.vue
@@ -1,24 +1,55 @@
 <template lang="pug">
   div.wrapAudios
-    h1 titletitle
-    form(method="post", enctype="multipart/form-data", action="./upload")
+    h1 {{msg}}
+    div
       input(type="file", name="file_to_upload")
       div
-      label name
-      input(type="text", name="songname")
+      label title
+      input(type="text", v-model="audio.title" v-on:input="update")
+      div
+      label filename
+      input(type="text", v-model="audio.filename")
       div
       label comment
-      input(type="text", name="comment")
-      input(type="submit")
+      input(type="text", v-model="audio.comment")
+      input(type="submit" @click="onClick").button
 </template>
+
+<style scoped>
+  input {
+    width: 300px;
+    margin-bottom: 25px;
+  }
+  .button {
+    width: 50px;
+    margin: 5px;
+  }
+</style>
 
 <script>
   export default {
     name: 'audios',
     data () {
       return {
-        msg: 'This is Upload2!!!'
+        msg: 'This is Upload2!!!',
+        audio: {
+          title: '',
+          filename: '',
+          comment: ''
+        }
       }
+    },
+    methods: {
+      onClick () {
+        console.log('click');
+      },
+      update (e) {
+        // タイトルが変更されたとき 例えばこういう処理する
+        this.$data.audio.filename = `${encodeURI(e.target.value)}.mp3`;
+      }
+    },
+    mounted () {
+      this.$data.audio.comment = 'Component mounted, and set comment...'
     }
   }
 </script>
